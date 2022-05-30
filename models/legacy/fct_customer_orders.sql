899bc1ad9559453851cc7c24dc74f492cb4bd00e
vineela.vce@gmail.com <vineela.vce@gmail.com>
2022-05-30 02:17:57 +0000

    refactoring

--- models/legacy/fct_customer_orders.sql
+++ models/legacy/fct_customer_orders.sql
@@ -1,47 +1,65 @@
-WITH paid_orders as (select Orders.ID as order_id,
-        Orders.USER_ID    as customer_id,
-        Orders.ORDER_DATE AS order_placed_at,
-            Orders.STATUS AS order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        C.FIRST_NAME    as customer_first_name,
-            C.LAST_NAME as customer_last_name
-    FROM raw.jaffle_shop.orders as Orders
-    left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-from raw.stripe.payment
-where STATUS <> 'fail'
-group by 1) p ON orders.ID = p.order_id
-left join raw.jaffle_shop.customers C on orders.USER_ID = C.ID ),
+with
 
-customer_orders 
-    as (select C.ID as customer_id
-        , min(ORDER_DATE) as first_order_date
-        , max(ORDER_DATE) as most_recent_order_date
-        , count(ORDERS.ID) AS number_of_orders
-    from raw.jaffle_shop.customers C 
-    left join raw.jaffle_shop.orders as Orders
-    on orders.USER_ID = C.ID 
-    group by 1)
+-- Import CTEs
 
-select
-    p.*,
-    ROW_NUMBER() OVER (ORDER BY p.order_id) as transaction_seq,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY p.order_id) as customer_sales_seq,
-    CASE WHEN c.first_order_date = p.order_placed_at
-    THEN 'new'
-    ELSE 'return' END as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-    FROM paid_orders p
-    left join customer_orders as c USING (customer_id)
-    LEFT OUTER JOIN 
-    (
-            select
-            p.order_id,
-            sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join paid_orders t2 on p.customer_id = t2.customer_id and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x on x.order_id = p.order_id
-    ORDER BY order_id
+customers as (
+
+  select * from {{ ref('stg_jaffle_shop__customers') }}
+
+),
+
+paid_orders as (
+
+  select * from {{ref('int_orders')}}
+
+),
+
+-- Final CTE
+
+final as (
+
+  select
+    paid_orders.order_id,
+    paid_orders.customer_id,
+    paid_orders.order_placed_at,
+    paid_orders.order_status,
+    paid_orders.total_amount_paid,
+    paid_orders.payment_finalized_date,
+    customers.customer_first_name,
+    customers.customer_last_name,
+
+    -- sales transaction sequence
+    row_number() over (order by paid_orders.order_placed_at, paid_orders.order_id) as transaction_seq,
+
+    -- customer sales sequence
+    row_number() over (
+        partition by paid_orders.customer_id
+        order by paid_orders.order_placed_at, paid_orders.order_id
+        ) as customer_sales_seq,
+
+    -- new vs returning customer
+    case 
+      when (
+      rank() over (
+        partition by paid_orders.customer_id
+        order by paid_orders.order_placed_at, paid_orders.order_id
+        ) = 1
+      ) then 'new'
+    else 'return' end as nvsr,
+
+    -- customer lifetime value
+    sum(paid_orders.total_amount_paid) over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) as customer_lifetime_value,
+
+    -- first day of sale
+    first_value(paid_orders.order_placed_at) over (
+      partition by paid_orders.customer_id
+      order by paid_orders.order_placed_at, paid_orders.order_id
+      ) as fdos
+    from paid_orders
+    left join customers on paid_orders.customer_id = customers.customer_id
+)
+
+select * from final
